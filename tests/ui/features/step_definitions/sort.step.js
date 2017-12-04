cdbed521b1d44ee22180a5327118c09a0b1f5ad4
CamilleINGOUF <ingouf.camille@gmail.com>
2017-12-04 12:05:53 +0100

    BDD second task done

--- tests/ui/features/step_definitions/sort.step.js
+++ tests/ui/features/step_definitions/sort.step.js
@@ -5,16 +5,55 @@
 const { Given, Then, When } = require('cucumber');
 const assert = require('assert');
 
-Given(/^The contact list is display$/, function(callback){
-    callback.pending();
+Given(/^The contact list is displayed$/, function(callback){
+    this.browser.visit("http://localhost:3000/", (err) => {
+        if(err) throw err;
+        this.browser.assert.text('h1', 'Contacts');
 
+        var header = this.browser.queryAll("#contacts table tr:first-child th");
+        var values = ["First name","Last name","Phones","Mails","Tags","Actions"];
+        header.forEach(function(item){
+            assert(values.indexOf(item.innerHTML) >= 0);
+        });
+
+        callback();
+    });
 });
 
 When(/^User clicks on sort button$/, function(callback){
-    callback.pending();
+    this.browser.visit("http://localhost:3000/", (err) => {
+        this.browser.clickLink('#button_sort', (err) => {
+            if(err) throw err;
+
+            var tab = this.browser.queryAll("#contacts table tr");
+            assert.equal(tab.length, 5);
+
+            callback();
+        });
+    });
 });
 
 Then(/^The list is sorted$/, function(callback){
-    callback.pending();
+    this.browser.visit("http://localhost:3000/", (err) => {
+        this.browser.clickLink('#button_sort', (err) => {
+            if(err) throw err;
 
+            var valid = true;
+            var oldLastName="";
+            var currentLastName = "";
+            var contactTab = this.browser.queryAll("#contacts table tbody tr td#cellLastName");
+            contactTab.forEach(function(item, i){
+                currentLastName = item.innerHTML
+                if(i > 1) {
+                    if(currentLastName < oldLastName)
+                        valid = false;
+                }
+                oldLastName = currentLastName;
+            });
+
+            assert(valid);
+
+            callback();
+        });
+    });
 });
